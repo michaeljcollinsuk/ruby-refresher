1c814a710031a9f596b58b064680405afb608bf6
Michael Collins <michaelj.collinsuk@gmail.com>
2016-05-18 09:56:04 +0100

    Completed upper limit of range question

--- lib/questions.rb
+++ lib/questions.rb
@@ -223,11 +223,13 @@ end
 # where 'special character' means anything apart from the letters
 # a-z (uppercase and lower) or numbers
 def check_a_string_for_special_characters(string)
+
 end
 
 # get the upper limit of a range. e.g. for the range 1..20, you
 # should return 20
 def get_upper_limit_of(range)
+  range.end
 end
 
 # should return true for a 3 dot range like 1...20, false for a
